d5c990b7b9ac366e0bc74ee6a4841ac220343265
RagAlest <jetblackarrow@gmail.com>
2019-01-10 01:54:37 +0900

    initial commit

---
+++ Coalescence/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Coalescence
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTargetExcel = new System.Windows.Forms.TextBox();
+            this.txtTargetFolder = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtColumns = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Output to";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ImgFolder";
+            // 
+            // txtTargetExcel
+            // 
+            this.txtTargetExcel.Location = new System.Drawing.Point(75, 13);
+            this.txtTargetExcel.Name = "txtTargetExcel";
+            this.txtTargetExcel.Size = new System.Drawing.Size(478, 19);
+            this.txtTargetExcel.TabIndex = 2;
+            // 
+            // txtTargetFolder
+            // 
+            this.txtTargetFolder.Location = new System.Drawing.Point(75, 46);
+            this.txtTargetFolder.Name = "txtTargetFolder";
+            this.txtTargetFolder.Size = new System.Drawing.Size(478, 19);
+            this.txtTargetFolder.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 429);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(443, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "This software is released under the WTFPL License, see http://www.wtfpl.net/about" +
+    "/.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(715, 429);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Early Access";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Columns";
+            // 
+            // txtColumns
+            // 
+            this.txtColumns.Location = new System.Drawing.Point(75, 79);
+            this.txtColumns.Name = "txtColumns";
+            this.txtColumns.Size = new System.Drawing.Size(32, 19);
+            this.txtColumns.TabIndex = 8;
+            this.txtColumns.Text = "5";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(13, 118);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(540, 308);
+            this.txtLog.TabIndex = 9;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(559, 118);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(52, 12);
+            this.lblStatus.TabIndex = 10;
+            this.lblStatus.Text = "Stand-by";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtColumns);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTargetFolder);
+            this.Controls.Add(this.txtTargetExcel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTargetExcel;
+        private System.Windows.Forms.TextBox txtTargetFolder;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtColumns;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Coalescence/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coalescence
+{
+    public partial class Form1 : Form
+    {
+        private dynamic app;
+        private dynamic books;
+        private dynamic book;
+        private dynamic sheets;
+        private dynamic sheet;
+        private dynamic range;
+        private dynamic shape;
+
+        private static char[] alphabet = Enumerable.Range('A', 'Z' - 'A' + 1).Select(i => (char)i).ToArray();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.txtTargetExcel.Text = "C:\\Users\\jetbl\\Desktop\\a.xlsx";
+            this.txtTargetFolder.Text = "C:\\png";
+            this.lblStatus.Text = "Stand-by";
+            this.lblStatus.ForeColor = Color.Green;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 感度良好524
+            this.lblStatus.Text = "Execute " + this.txtTargetFolder.Text;
+            this.lblStatus.ForeColor = Color.Salmon;
+            this.CreateExcelFile(this.txtTargetExcel.Text, this.CreateFileList(this.txtTargetFolder.Text));
+        }
+
+        private void CreateExcelFile(string excelFile, string[,] imgList)
+        {
+            string pic = string.Empty;
+            string position = string.Empty;
+            try
+            {
+                Type excelApp = Type.GetTypeFromProgID("Excel.Application");
+                app = Activator.CreateInstance(excelApp);
+
+                app.DisplayAlerts = false;
+
+                books = app.WorkBooks;
+                book = books.Add();
+                sheets = book.Sheets;
+                sheet = sheets["Sheet1"];
+
+                for (int y = 0; y < imgList.GetLength(1); ++y)
+                {
+                    for (int x = 0; x < imgList.GetLength(0); ++x)
+                    {
+                        position = string.Empty;
+                        pic = System.IO.Path.Combine(this.txtTargetFolder.Text, imgList[x, y]);
+                        this.txtLog.Text += "pasting " + pic;
+
+                        if(x > 1)
+                        {
+                            // 1枚の幅 20 * 何列目か / アルファベットの数 = 何週目のアルファベットか
+                            // (計算結果(商)が0 = アルファベット1文字, 計算結果(商)が1 = AA-AZ, 2 = BA-BZ)
+                            position = alphabet[20 * x / 25 - 1].ToString();
+                            position += alphabet[20 * x % 25].ToString();
+                        }
+                        else
+                        {
+                            position += alphabet[20 * x % 25].ToString();
+                        }
+
+                        this.txtLog.Text += " to Cell" + string.Concat(position, y * 15) + "..." + Environment.NewLine;
+
+                        range = sheet.Range[string.Concat(position, (y * 25 + 1).ToString())];
+
+                        double left = range.Left;
+                        double top = range.Top;
+                        double width = 0;
+                        double height = 0;
+
+                        shape = sheet.Shapes.AddPicture(pic, true, true, left, top, width, height);
+
+                        shape.ScaleHeight(1.0, true);
+                        shape.ScaleWidth(1.0, true);
+                    }
+                }
+
+                book.SaveAs(excelFile);
+                book.Close();
+                app.Quit();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.StackTrace);
+            }
+            finally
+            {
+                this.releaseObject((object)shape);
+                this.releaseObject((object)range);
+                this.releaseObject((object)sheet);
+                this.releaseObject((object)sheets);
+                this.releaseObject((object)book);
+                this.releaseObject((object)books);
+                this.releaseObject((object)app);
+                this.lblStatus.Text = "Stand-by";
+                this.lblStatus.ForeColor = Color.Green;
+            }
+        }
+
+        private string[,] CreateFileList(string folderName)
+        {
+            System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(folderName);
+            System.IO.FileInfo[] pngFiles = directory.GetFiles("*.png", System.IO.SearchOption.AllDirectories);
+            
+            //どっちか、もしくはいい感じのやつ
+            //var orderedFiles = pngFiles.OrderBy(x => x.FullName);
+            var orderedFiles = pngFiles.OrderBy(x => x.CreationTime);
+
+            int columnCount = Convert.ToInt32(this.txtColumns.Text);
+            //int rowCount = Convert.ToInt32(pngFiles.Length / columnCount);
+            int rowCount = Convert.ToInt32(orderedFiles.Count() / columnCount);
+            int fileCount = 0;
+
+            string[,] imgList = new string[columnCount, rowCount];
+
+            for (int y = 0; y < rowCount; ++y)
+            {
+                for (int x = 0; x < columnCount; ++x)
+                {
+                    imgList[x, y] = orderedFiles.ToArray()[fileCount].Name;//
+                    fileCount++;
+                }
+            }
+
+            return imgList;
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                Marshal.FinalReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception e)
+            {
+                obj = null;
+                MessageBox.Show("COMオブジェクト解放失敗 : " + e.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
